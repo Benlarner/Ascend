9ef71fc821d58424fd78314e9b9f526f31af6612
omar gubran <omarsalehjubran@gmail.com>
2020-10-24 14:58:42 -0400

    Navbar + Footer

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <Foot>
+            <div className="footer-bottom">
+        <small> Copyright &copy; <strong>Ascend</strong> </small>
+
+            </div>
+        </Foot>
     )
 }
 
-export default Footer
+export default Footer;
+
+
+const Foot = styled.footer`
+  background-color: #343b3f;
+  height: 50px;
+  color: white;
+  padding: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+`;
+
+
--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../Data/Assests/Logo.jpg'
 
 function Navbar() {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
+    return (
+        <Nav>
+            <img src={Logo} alt=""/>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </Nav>
+    )
 }
 
 export default Navbar
+
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  z-index: 100;
+  align-items: center;
+  position: sticky;
+  margin-bottom: 10px;
+  top: 0;
+  background-color: white;
+  padding: 15px;
+  ul {
+      text-decoration: none;
+  }
+  li {
+    display: inline;
+  margin: 20px;
+  font-size: 1.4em;
+  cursor: pointer;
+  }
+  img {
+    height: 40px;
+  }
+`;
