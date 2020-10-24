30f248071c12ef0f1c279690f8d2b1a16cfa70f0
omar gubran <omarsalehjubran@gmail.com>
2020-10-24 14:55:20 -0400

    Structured App + Added dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import './App.css';
+import Home from "./Components/HomePage/Home";
+import Products from "./Components/Products/Products";
+import ProductPage from "./Components/ProductPage/ProductPage";
+import Contact from "./Components/Contact/Contact";
+import ShoppingCart from './Components/ShoppingCart/ShoppingCart';
+import Footer from "./Components/Layout/Footer";
+import Navbar from "./Components/Layout/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Products" component={Products} />
+        <Route exact path="/ProductPage" component={ProductPage} />
+        <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/Cart" component={ShoppingCart} />
+    </Switch>
+    <Footer/>
+    </>
   );
 }
 
